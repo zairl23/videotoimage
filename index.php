4401602af17e3d9e96be16a1948aef3f00d807ba
zairl23 <756127792@qq.com>
2013-08-01 15:35:38 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
 // form for input about config
-
-  
 ?>
 <!doctype html>
 <head>
   <title>video to image</title>
 </head>
+
+<body>
+  <div id='form'>
+    <form name='videourl' id='videlurl' method='post' action='covert.php'>
+     <p>vedio url:</p>
+     <input type='text' id='url' name='url'/>
+     <p></p>
+      <input type='submit' value='发送'/>
+    </form>
+  </div>
+</body>
 </html>
